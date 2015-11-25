329b1d1710f4406e8be0966fb32ab6183437a317
codingonHP <vishal_24_anand@hotmail.com>
2015-11-25 10:07:18 +0530

    Added CSS and sytles and ui routing

--- app/global/globalSettings.js
+++ app/global/globalSettings.js
@@ -2,17 +2,34 @@
     'use strict';
     angular.module('menuMdl')
         .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise('/index');
-            $stateProvider.state('index', {
-                url: '/index',
-                views: {
-                    header: {
-                        templateUrl: '/views/shared/_header.html'
-                    },
-                    nav: {
-                        templateUrl: '/views/shared/_nav.html'
+            $urlRouterProvider.otherwise('/diner/home');
+
+            $stateProvider
+                .state('dinerHome', {
+                    url: '/diner/home',
+                    views: {
+
+                        '@': {
+                            templateUrl: '/views/shared/_master.html' //parent view that is loaded on page load in index
+                        },
+                        'header@dinerHome': {
+                            templateUrl: '/views/shared/_header.html'
+                        },
+                        'nav@dinerHome': {
+                            templateUrl: '/views/shared/_nav.html'
+                        },
+                        '@dinerHome': {
+                            templateUrl: '/views/partials/breakfast/breakfast.html'
+                        }
                     }
-                }
-            });
+
+                }).state('dinerHome.breakfast', {
+                    url: '/breakfast',
+                    views: {
+                        'breakfastDetails@dinerHome': {
+                            templateUrl: '/views/partials/breakfast/fruitLoops.html'
+                        }
+                    }
+                });
      }]);
 }());
